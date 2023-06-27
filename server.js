bb10b00181d1d3e0bf26b2ee2e9d25eb33cea265
Jacob Rubino <jacobrubino@gmail.com>
2023-06-27 15:32:27 -0400

    adding handlebars to the server.js, and helpers aka formatted date for posts to the utils

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const session = require('express-session');
 const routes = require('./controllers');
 const path = require('path');
 const dotenv = require('dotenv');
+const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -28,6 +30,10 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs({ helpers }));
+app.set('view engine', 'handlebars');
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
