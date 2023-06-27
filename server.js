224572942b95ecf2ac148d3fa53aa36c5e735fe3
Jacob Rubino <jacobrubino@gmail.com>
2023-06-27 15:17:45 -0400

    working on the server, gotta getr handlebars to work so i can start working on css and testing things out easily

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-
+const path = require('path');
+const dotenv = require('dotenv');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: 'Super secret cookie',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+    secure: false
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
